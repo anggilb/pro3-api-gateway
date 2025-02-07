722f2c2467fdc8a98c770725d643e1b079301d57
Ángel Javier Gil Bécares <angeljavgil@gmail.com>
2025-02-07 17:44:00 +0100

    Corregido problema en auth-service-api.

--- auth-service-api/src/main/java/com/example/auth_service_api/AuthServiceApiApplication.java
+++ auth-service-api/src/main/java/com/example/auth_service_api/AuthServiceApiApplication.java
@@ -3,11 +3,9 @@ package com.example.auth_service_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.auth_service_api", "com.example.users_service_api"})
-@EnableJpaRepositories(basePackages = "com.example.users_service_api.repositories")
 public class AuthServiceApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApiApplication.class, args);
