74325e964e7fd74ed6f35f3c082141293244db9a
Ángel Javier Gil Bécares <angeljavgil@gmail.com>
2025-02-07 04:12:35 +0100

    Modificación faltante.

--- users-service-api/src/main/java/com/example/users_service_api/config/JwtAuthFilter.java
+++ users-service-api/src/main/java/com/example/users_service_api/config/JwtAuthFilter.java
@@ -4,24 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import com.example.users_service_api.repositories.UserRepository;
-import com.example.users_service_api.commons.entities.UserModel;
 
 import java.io.IOException;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
-    private final UserRepository userRepository;
-
-    public JwtAuthFilter(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -33,9 +22,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             return;
         }
 
-        userRepository.findById(Long.valueOf(userIdHeader)).ifPresent(userDetails -> {
-            request.setAttribute("userIdRequest", userDetails.getUserId());
-        });
+        request.setAttribute("userIdRequest", userIdHeader);
 
         filterChain.doFilter(request, response);
     }
--- users-service-api/src/main/java/com/example/users_service_api/config/SecurityConfig.java
+++ users-service-api/src/main/java/com/example/users_service_api/config/SecurityConfig.java
@@ -1,20 +1,14 @@
 package com.example.users_service_api.config;
 
-import com.example.users_service_api.repositories.UserRepository;
-import com.example.users_service_api.service.impl.UserDetailsImpl;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,19 +16,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan(basePackages = {"com.example.auth_service_api", "com.example.users_service_api"})
 public class SecurityConfig {
-    private final UserDetailsService userDetailsService;
     private final JwtAuthFilter jwtAuthFilter;
 
-    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthFilter jwtAuthFilter) {
-       this.userDetailsService = userDetailsService;
-       this.jwtAuthFilter = jwtAuthFilter;
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
-        return new UserDetailsImpl(userRepository);
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
@@ -42,22 +28,13 @@ public class SecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request.requestMatchers("v1/**", "/swagger-ui/**", "/swagger-ui.html")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated())
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
     }
 
     @Bean
